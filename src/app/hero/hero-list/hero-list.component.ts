94e36680224f1aef2aed6d508df6fe81e78401ff
edward zareba <tomaszstolarczyk86@gmail.com>
2017-05-14 22:44:56 +0200

    heroes service

--- src/app/hero/hero-list/hero-list.component.ts
+++ src/app/hero/hero-list/hero-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "app/hero/hero";
+import { HeroService } from "app/hero/hero-service";
 
 @Component({
   selector: 'app-hero-list',
@@ -10,28 +11,20 @@ export class HeroListComponent implements OnInit {
 
   selectedHero: Hero;
   title = 'Tour of Heroes';
+  heroes: Hero[];
 
-  heroes: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
-
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  }
 
 }
--- src/app/hero/hero.module.ts
+++ src/app/hero/hero.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HeroRoutingModule } from './hero-routing.module';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { HeroDetailComponent } from "app/hero/hero-detail/hero-detail.component";
+import { HeroService } from "app/hero/hero-service";
 
 @NgModule({
   imports: [
@@ -12,6 +13,7 @@ import { HeroDetailComponent } from "app/hero/hero-detail/hero-detail.component"
     HeroRoutingModule,
     FormsModule
   ],
-  declarations: [HeroListComponent, HeroDetailComponent]
+  declarations: [HeroListComponent, HeroDetailComponent],
+  providers: [HeroService]
 })
 export class HeroModule { }
